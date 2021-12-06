b56e95e0043591a7eaeb0df15b17018af64666fd
David Black <tannv85@gmail.com>
2021-12-06 19:42:26 +0700

    First Commit

---
+++ src/test/java/com/tannv85/productservices/ProductServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tannv85.productservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
